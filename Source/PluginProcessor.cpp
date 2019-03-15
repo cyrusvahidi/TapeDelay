e0dabb8a640ec661f58b079de00a55c3ff9afff3
Cyrus Vahidi <cyrusvahidi@gmail.com>
2019-03-15 15:36:11 +0000

    Init commit & APVTS param setup

---
+++ Source/PluginProcessor.cpp
@@ -0,0 +1,201 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+TapeDelayAudioProcessor::TapeDelayAudioProcessor()
+#ifndef JucePlugin_PreferredChannelConfigurations
+     : AudioProcessor (BusesProperties()
+                     #if ! JucePlugin_IsMidiEffect
+                      #if ! JucePlugin_IsSynth
+                       .withInput  ("Input",  AudioChannelSet::stereo(), true)
+                      #endif
+                       .withOutput ("Output", AudioChannelSet::stereo(), true)
+                     #endif
+                       ),
+    state(
+        new AudioProcessorValueTreeState(*this, nullptr, "Params",
+            {
+                std::make_unique<AudioParameterFloat>("delayTime", "Delay Time", NormalisableRange<float>(0, 2, 0.01f), 0.5f),
+                std::make_unique<AudioParameterFloat>("wetMix", "Dry/Wet", NormalisableRange<float>(0, 1, 0.01f), 0.5f),
+                std::make_unique<AudioParameterFloat>("tapMix", "Tap Mix", NormalisableRange<float>(0, 1, 0.01f), 0.5f),
+                std::make_unique<AudioParameterFloat>("feedback", "Feedback", NormalisableRange<float>(0, 1, 0.01f), 0.5f)
+            }
+        )
+    )
+#endif
+{
+}
+
+TapeDelayAudioProcessor::~TapeDelayAudioProcessor()
+{
+}
+
+//==============================================================================
+const String TapeDelayAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool TapeDelayAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool TapeDelayAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool TapeDelayAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double TapeDelayAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int TapeDelayAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int TapeDelayAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void TapeDelayAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const String TapeDelayAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void TapeDelayAudioProcessor::changeProgramName (int index, const String& newName)
+{
+}
+
+//==============================================================================
+void TapeDelayAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    // Use this method as the place to do any pre-playback
+    // initialisation that you need..
+}
+
+void TapeDelayAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool TapeDelayAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    if (layouts.getMainOutputChannelSet() != AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
+
+void TapeDelayAudioProcessor::processBlock (AudioBuffer<float>& buffer, MidiBuffer& midiMessages)
+{
+    ScopedNoDenormals noDenormals;
+    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumOutputChannels = getTotalNumOutputChannels();
+
+    // In case we have more outputs than inputs, this code clears any output
+    // channels that didn't contain input data, (because these aren't
+    // guaranteed to be empty - they may contain garbage).
+    // This is here to avoid people getting screaming feedback
+    // when they first compile a plugin, but obviously you don't need to keep
+    // this code if your algorithm always overwrites all the output channels.
+    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
+
+    // This is the place where you'd normally do the guts of your plugin's
+    // audio processing...
+    // Make sure to reset the state if your inner loop is processing
+    // the samples and the outer loop is handling the channels.
+    // Alternatively, you can process the samples with the channels
+    // interleaved by keeping the same state.
+    for (int channel = 0; channel < totalNumInputChannels; ++channel)
+    {
+        auto* channelData = buffer.getWritePointer (channel);
+
+        // ..do something to the data...
+    }
+}
+
+//==============================================================================
+bool TapeDelayAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+AudioProcessorEditor* TapeDelayAudioProcessor::createEditor()
+{
+    return new TapeDelayAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+void TapeDelayAudioProcessor::getStateInformation (MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+}
+
+void TapeDelayAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new TapeDelayAudioProcessor();
+}
